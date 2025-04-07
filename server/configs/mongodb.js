a81d236244a984c7cb9c1240a135833a88971f9e
rahultejus <rahultejus123@gmail.com>
2025-04-07 14:41:30 +0530

    webhook update

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,14 +1,30 @@
-import mongoose from 'mongoose'
+import  mongoose from 'mongoose'
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected Successfully!");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err.message);
+    process.exit(1); // Exit if DB connection fails
+  }
+};
+
+export default connectDB;
+
+// import mongoose from 'mongoose'
 
 
-// Connect to MongoDB database
+// // Connect to MongoDB database
 
-const connectDB=async()=>{
-  mongoose.connection.on('connected',()=>
-  console.log('Database Connected'))
+// const connectDB=async()=>{
+//   mongoose.connection.on('connected',()=>
+//   console.log('Database Connected'))
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
-}
+//   await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+// }
 
-export default connectDB
+// export default connectDB
 
